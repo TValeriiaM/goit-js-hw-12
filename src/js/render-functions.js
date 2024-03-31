310f9452429263c04f4513e7366dc1872bb01bdd
TValeriiaM <149121749+TValeriiaM@users.noreply.github.com>
2024-03-31 02:41:46 +0200

    Initial commit

---
+++ src/js/render-functions.js
@@ -0,0 +1,51 @@
+import { galleryImages } from '../main';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+
+const lightbox = new SimpleLightbox('.gallery-link', {
+  captionsData: 'alt',
+});
+
+export function renderImages(data) {
+  const galleryMarkup = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            src="${webformatURL}"
+            data-source="${largeImageURL}"
+            alt="${tags}"
+          />
+          <ul class="gallery-description">
+          <li class="gallery-dscr_item"><h3>Likes</h3><p>${likes}</p>
+          </li>
+          <li class="gallery-dscr_item"><h3>Views</h3><p>${views}</p>
+            </li>
+            <li class="gallery-dscr_item"><h3>Comments</h3><p>${comments}</p>
+              </li>
+              <li class="gallery-dscr_item"><h3>Downloads</h3><p>${downloads}</p>
+                </li>
+          </ul>
+        </a>
+      </li>`;
+      }
+    )
+    .join('');
+
+  galleryImages.insertAdjacentHTML('beforeend', galleryMarkup);
+
+  lightbox.refresh();
+}
---
+++ src/main.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { getImage } from './js/pixabay-api';
+import { renderImages } from './js/render-functions';
+
+const searchForm = document.querySelector('.form');
+export const galleryImages = document.querySelector('.gallery');
+const loadMove = document.querySelector('.loader');
+loadMove.style.display = 'none';
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  galleryImages.innerHTML = '';
+  const query = event.target.elements.search.value.trim();
+
+  if (query) {
+    loadMove.style.display = 'flex';
+
+    getImage(query)
+      .then(data => {
+        if (!data.hits.length) {
+          iziToast.error({
+            title: '❌',
+            icon: '',
+            message: `Sorry, there are no images matching your search query. Please, try again!`,
+          });
+        }
+        renderImages(data.hits);
+        loadMove.style.display = 'none';
+      })
+      .catch(error => {
+        console.log(error);
+        loadMove.style.display = 'none';
+      });
+  }
+  event.target.reset();
+});
